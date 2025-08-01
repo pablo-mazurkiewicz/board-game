a3b77b07fae64efd1518fb09594b77b9e3ec7cb2
pablo-mazurkiewicz <pmazurkiewicz01@gmail.com>
2025-08-01 16:38:42 +0200

    vite

--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css"; // or wherever your globals.css file is
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
